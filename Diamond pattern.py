c36f9219b1501f63cf55164c7ad29ff389d528fd
adh-123 <chiranjeevitellagorla2004@gmail.com>
2026-02-18 16:40:21 +0530

    Complete diamond pattern printing logic
    Fix indentation and add bottom half of diamond pattern.

--- Diamond pattern.py
+++ Diamond pattern.py
@@ -2,6 +2,12 @@ n=5
 for i in range(1,n+1):
     for j in range(1,n-i):
         print(" ",end=" ")
-        for k in range(2*i-1):
+    for k in range(2*i-1):
+            print("*",end=' ')
+        print()
+for i in range(n,0,n-1):
+    for j in range(1,n-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):
             print("*",end=' ')
         print()
